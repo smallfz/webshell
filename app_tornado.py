e02f32ea37ce98e268440b73f65a0e095cd502da
smallfz <small.fz@gmail.com>
2016-06-21 20:14:59 +0800

    commandline supports; daemonize

--- app_tornado.py
+++ app_tornado.py
@@ -9,21 +9,47 @@ if _dir not in sys.path:
 from tornado.httpserver import HTTPServer
 from tornado.wsgi import WSGIContainer
 from tornado.ioloop import IOLoop
+import argparse as ap
+
+try:
+    import daemonize
+except ImportError:
+    daemonize = None
 
 
 from app import app
 
+config = {
+    'port': 9990
+}
 
-
-if __name__ == '__main__':
-    port = 9990
-    _argv = sys.argv[1:]
-    if _argv and _argv[0].isdigit():
-        port = int(_argv[0])
-
+def server():
+    port = config['port']
     print 'serving port: %s' % port
-
+    return
     http_server = HTTPServer(WSGIContainer(app))
     http_server.listen(port, address='127.0.0.1')
     IOLoop.instance().start()
+    
+
+if __name__ == '__main__':
+    parser = ap.ArgumentParser(description=u'Run a webshell server.')
+    parser.add_argument('cmd', choices=['start', 'stop'])
+    parser.add_argument('--port',
+                        default=config['port'],
+                        type=int,
+                        required=False,
+                        help='http port, default 9990')
+
+    args = parser.parse_args()
+    config['port'] = args.port
+    if daemonize and sys.platform != 'win32':
+        pid = '/var/run/webshell.pid'
+        daemon = Daemonize(app='webshell', pid=pid, action=server)
+        if args.cmd == 'start':
+            daemon.start()
+        elif args.cmd == 'stop':
+            daemon.exit()
+    else:
+        server()
 
